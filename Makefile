1369d2d045cc03ebe83b8869efcf792f59080652
AndreiIlin <andrei362065@gmail.com>
2022-06-24 00:04:04 +0300

    delete userSlice

--- Makefile
+++ Makefile
@@ -2,3 +2,9 @@ start-backend:
 		cd server && npm start
 start-frontend:
 		cd application && npm start
+install:
+		cd server
+		npm ci
+		cd ..
+		cd application
+		npm ci
--- application/src/slices/index.js
+++ application/src/slices/index.js
@@ -1,12 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import channelsReducer from './channelsSlice.js';
-import usersReducer from './usersSlice.js';
 import messagesReducer from './messagesSlice.js';
 
 export default configureStore({
   reducer: {
     channels: channelsReducer,
-    users: usersReducer,
     messages: messagesReducer,
   },
 });
